9d8fae5de16d92db1dd6ea62fad84f1d35bf3725
Lun-learn <aa455931143@gmail.com>
2020-12-05 23:09:56 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+        Person p = new Person();
+        p.hello();
+        Integer age2 = 19;
+        Character c2 = 'a';
+
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
+    }
+
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,24 @@
+package com.tom
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+
+    var age = 19
+    age = 20
+    var weight = 66.5f
+    var name : String
+    name = "Hank"
+}
+
+class Human{
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
+
+
+//git config --global user.email "aa455931143@gmail.com"
+//git config --global user.name "Lun-learn"
